f60a3cf3534c9e00242908776f8f4f9adeea3fc0
SAMUDESHI <mishaludeshi96@gmail.com>
2023-01-30 10:36:09 +0530

    1st Commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by saavajgroup on 30/01/23.
+//
+
+import Foundation
+
+public struct SDK{
+    public static func doSomeWork(){
+        print("My 1st Custom Framework")
+    }
+}
